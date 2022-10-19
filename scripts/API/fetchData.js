e4522c22cb4255a24833d26dcb13e3e55b7cda75
S-Syrichko <s-syrichko@protonmail.com>
2022-10-19 21:26:14 +0200

    test: changed path in fetchData.js

--- scripts/API/fetchData.js
+++ scripts/API/fetchData.js
@@ -1,14 +1,14 @@
 import { sortMedia } from "../utils/functions.js";
 
 export async function getAllPhotographers() {
-  return fetch("/data/photographers.json")
+  return fetch("data/photographers.json")
     .then((res) => res.json())
     .then((res) => res.photographers)
     .catch((err) => console.log("Fetch error occurs", err));
 }
 
 export async function getPhotographerByUserID(userID) {
-  return fetch("/data/photographers.json")
+  return fetch("data/photographers.json")
     .then((res) => res.json())
     .then((res) => {
       const photographer = res.photographers.filter(
@@ -20,7 +20,7 @@ export async function getPhotographerByUserID(userID) {
 }
 
 export async function getMediaByUserID(userID) {
-  return fetch("/data/photographers.json")
+  return fetch("data/photographers.json")
     .then((res) => res.json())
     .then((res) => {
       const mediaList = res.media.filter(
